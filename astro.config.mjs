51ccbe0204b0461e62bc8a4cd1674301239f9870
ssgsg <986843038@qq.com>
2024-09-29 17:11:02 +0800

    完结撒花！最后把finished改为true的那一刻，成就感达到了巅峰！

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,9 @@
 import { defineConfig } from 'astro/config';
 
+import preact from "@astrojs/preact";
+
 // https://astro.build/config
 export default defineConfig({
-    site: "https://ssg-blog.netlify.app"
+  site: "https://ssg-blog.netlify.app",
+  integrations: [preact()]
 });
